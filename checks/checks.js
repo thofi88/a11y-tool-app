ce41a529327ca09cf2b6c3747fbe8a659f42c28e
Thomas Fichna <thomasfichna@gmail.com>
2020-07-06 15:10:15 +0200

    first try with check a website

--- checks/checks.js
+++ checks/checks.js
@@ -4,6 +4,38 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const request = require('request');
+request('http://localhost:8000/websites/', { json: true }, (err, res, body) => {
+
+  websites = body;
+  console.log();
+  for (let i = 0; i < websites.length; i++) {
+
+    console.log(`http://localhost:8000/websiteOneCheck/${websites[i].id}`);
+    console.log();
+
+
+    const requestCheck = require('request');
+    requestCheck(`http://localhost:8000/websiteCheck/${websites[i].id}`, { json: true }, (err, res, body) => {
+      checks = body;
+      for (let i = 0; i < checks.length; i++) {
+        console.log(checks[i].url);
+      }
+
+
+});
+
+  }
+});
+
+// const request = require('request');
+// request('http://localhost:8000/websites/', { json: true }, (err, res, body) => {
+//   if (err) { return console.log(err); }
+//   console.log(body.url);
+//   console.log(body.explanation);
+// });
+
+
 app.use(function(req, res, next) {
 
   res.header("Access-Control-Allow-Origin", "*");
